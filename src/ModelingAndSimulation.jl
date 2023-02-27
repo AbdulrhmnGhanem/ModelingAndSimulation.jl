ca4f04195b0697d80d497084a839cfa8fe2418fd
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2023-02-27 09:19:18 +0200

    Create `State` struct

--- src/ModelingAndSimulation.jl
+++ src/ModelingAndSimulation.jl
@@ -1,5 +1,40 @@
 module ModelingAndSimulation
+using DataFrames
+using ReusePatterns
 
-# Write your package code here.
+struct State <: AbstractDataFrame
+    df::DataFrame
+end
 
+@forward((State, :df), DataFrame)
+
+function Base.getproperty(state::State, key::Symbol)
+    val = state[!, key]
+    if isa(val, AbstractVector) && length(val) == 1
+        return val[1]
+    end
+    return val
+end
+
+function Base.setproperty!(state::State, key::Symbol, x)
+    if !isa(x, AbstractVector)
+        val = [x]
+    end
+    state[!, key] = val
+end
+
+function Base.setindex!(state::State, x, key::Symbol)
+    val = x
+    if !isa(x, AbstractVector)
+        val = [x]
+    end
+    state[!, key] = val
+end
+
+State(; args...) = State(DataFrame(; args...))
+
+# Alias TimeSeries to DataFrame
+const TimeSeries = DataFrame
+
+flip(p=0.5) = rand() < p
 end
